0b6351ceec70572b3740579878e171a5e98f3949
Sachin Kumar <sachinrajputlive@gmail.com>
2024-12-04 17:58:15 +0530

    [gh-pages] removed productionSourceMaps from next.config.ts to resolve build issue

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ const nextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/startup-app' : '/startup-app', // Use '/startup-app' in production mode
   assetPrefix: process.env.NODE_ENV === 'production' ? '/startup-app/' : '/startup-app', // Use '/startup-app/' in production
   trailingSlash: true,
-  productionSourceMaps: false,
 };
 
 module.exports = nextConfig;
