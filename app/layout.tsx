e732f21aeed3c413cdb9aedf4ae9e400deee77a3
Sachin Kumar <sachinrajputlive@gmail.com>
2024-12-04 17:45:13 +0530

    [main] routing issue fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { APP_DISCRIPTION, APP_NAME, APP_ROUTES } from '@/utils/constants';
 import './globals.css';
 import { ReactNode } from 'react';
+import LinkComponent from '@/components/link-components';
 
 export const metadata = {
   title: APP_NAME,
@@ -13,15 +14,21 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body className="bg-gray-100 text-gray-800 min-h-screen flex flex-col">
         <header className="bg-blue-600 text-white py-4">
           <nav className="container mx-auto flex justify-center space-x-4">
-            <a href={APP_ROUTES.HOME} className="hover:underline font-medium">
-              Home
-            </a>
-            <a href={APP_ROUTES.REGISTER} className="hover:underline font-medium">
-              Register
-            </a>
-            <a href={APP_ROUTES.DASHBOARD} className="hover:underline font-medium">
-              Dashboard
-            </a>
+            <LinkComponent
+              path={APP_ROUTES.HOME}
+              classes="hover:underline font-medium"
+              text="Home"
+            />
+            <LinkComponent
+              path={APP_ROUTES.REGISTER}
+              classes="hover:underline font-medium"
+              text="Register"
+            />
+            <LinkComponent
+              path={APP_ROUTES.DASHBOARD}
+              classes="hover:underline font-medium"
+              text="Dashboard"
+            />
           </nav>
         </header>
         <main className="flex-1 flex flex-col items-center justify-center p-4">
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 const nextConfig = {
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/startup-app' : '', // Use '/startup-app' in production mode
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/startup-app/' : '', // Use '/startup-app/' in production
+  // output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/startup-app' : '/startup-app', // Use '/startup-app' in production mode
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/startup-app/' : '/startup-app', // Use '/startup-app/' in production
   trailingSlash: true,
+  productionSourceMaps: false,
 };
 
 module.exports = nextConfig;
