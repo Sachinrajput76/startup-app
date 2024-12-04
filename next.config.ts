fa7b56f555c6dba56e707fd8628368f133f7d9a8
Sachin Kumar <sachinrajputlive@gmail.com>
2024-12-04 19:09:33 +0530

    [gh-pages] added all the files by command git checkout main -- .

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ const nextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/startup-app' : '/startup-app', // Use '/startup-app' in production mode
   assetPrefix: process.env.NODE_ENV === 'production' ? '/startup-app/' : '/startup-app', // Use '/startup-app/' in production
   trailingSlash: true,
+  productionSourceMaps: false,
 };
 
 module.exports = nextConfig;
