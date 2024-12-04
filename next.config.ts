1f8aa242848d01a4f812dff934395edbddc350c8
Sachin Kumar <sachinrajputlive@gmail.com>
2024-12-04 19:24:44 +0530

    Updates

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-const nextConfig = {
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/startup-app' : '/startup-app', // Use '/startup-app' in production mode
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/startup-app/' : '/startup-app', // Use '/startup-app/' in production
-  trailingSlash: true,
-  productionSourceMaps: false,
-};
-
-module.exports = nextConfig;
